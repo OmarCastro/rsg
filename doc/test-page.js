5e87795e7f36c5d2ad56f3c1cb1e1da832f3361e
Omar Castro <omar.castro.360@gmail.com>
2023-08-18 23:01:46 +0100

    src: redraw on resize

--- doc/test-page.js
+++ doc/test-page.js
@@ -3,9 +3,37 @@ import { drawGrid, createPoly } from "../src/utils/hexagon-grid.render.js";
 /** @type {HTMLCanvasElement} */
 const canvas = document.querySelector(".game")
 
+
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas 
+ */
+const redraw = (canvas) => {
+    const context = canvas.getContext("2d")
+    drawGrid(1,1,14,15, createPoly(6), context)
+}
+
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas 
+ */
+const updateCanvasSize = (canvas, rect) => {
+    canvas.width  = rect.width;
+    canvas.height = rect.height;
+
+    redraw(canvas)
+}
+
 const context = canvas.getContext("2d")
 
-context.canvas.width  = window.innerWidth;
-context.canvas.height = window.innerHeight;
+updateCanvasSize(canvas, canvas.getBoundingClientRect())
 
-drawGrid(1,1,14,15, createPoly(6), context)
+const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+        updateCanvasSize(entry.target, entry.contentRect)        
+    }
+});
+
+resizeObserver.observe(canvas)
